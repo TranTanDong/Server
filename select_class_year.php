876cd4893aff88ac85094036c5c2d55470370d21
TranTanDong <trantandong96@gmail.com>
2018-11-28 00:32:22 +0700

    New file select class year

--- select_class_year.php
+++ select_class_year.php
@@ -7,23 +7,27 @@
 	$data = mysqli_query($conn,$query);
 	while ($row = mysqli_fetch_assoc($data)) {
 		array_push($arrayclassyear, new ClassYear(
-			$row['s_id'],
-			$row['s_name'],
-			$row['s_createday'],
+			$row['c_id'],
+			$row['c_name'],
+			$row['c_idsemester'],
+			$row['sm_name'],
+			$row['sm_year'],
 			$row['st_codeuser'],
 			$row['st_id'])
 		);
 	}
 
-	echo json_encode($arraysubject);
+	echo json_encode($arrayclassyear);
 
 
 
 	class ClassYear{
-		function ClassYear($id,$name,$createday,$codeuser,$idst){
-			$this->id=$id;
-			$this->name=$name;
-			$this->createday=$createday;
+		function ClassYear($idclass,$nameclass,$idsemester,$namesemester,$year,$codeuser,$idst){
+			$this->idclass=$idclass;
+			$this->nameclass=$nameclass;
+			$this->idsemester=$idsemester;
+			$this->namesemester=$namesemester;
+			$this->year=$year;
 			$this->codeuser=$codeuser;
 			$this->idst=$idst;
 		}
